632b6165e6648ac6cf74e4049fb8a1dacda12037
Sohail Koutari <mirocairo15@gmail.com>
2024-05-02 16:21:18 +0100

    update

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -7,19 +7,19 @@
             <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
                 <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
-                    {{ __('Confirm Password') }}
+                    {{ __('Confirmez le mot de passe') }}
                 </header>
 
                 <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8" method="POST" action="{{ route('password.confirm') }}">
                     @csrf
 
                     <p class="leading-normal text-gray-500">
-                        {{ __('Please confirm your password before continuing.') }}
+                        {{ __('Veuillez confirmer votre mot de passe avant de continuer.') }}
                     </p>
 
                     <div class="flex flex-wrap">
                         <label for="password" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-                            {{ __('Password') }}:
+                            {{ __('Mot de passe') }}:
                         </label>
 
                         <input id="password" type="password"
@@ -36,13 +36,13 @@
                     <div class="flex flex-wrap justify-center items-center space-y-6 pb-6 sm:pb-10 sm:space-y-0 sm:justify-between">
                         <button type="submit"
                         class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700 sm:w-auto sm:px-4 sm:order-1">
-                            {{ __('Confirm Password') }}
+                            {{ __('Confirmez le mot de passe') }}
                         </button>
 
                         @if (Route::has('password.request'))
                         <a class="mt-4 text-xs text-blue-500 hover:text-blue-700 whitespace-no-wrap no-underline hover:underline sm:text-sm sm:order-0 sm:m-0"
                             href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
+                            {{ __('Mot de passe oublié?') }}
                         </a>
                         @endif
                     </div>
